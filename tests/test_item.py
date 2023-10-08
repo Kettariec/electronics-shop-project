8c1c1abdaa434104083b3cec509a1f27fba4059d
Konstantin Senkin <kettariec@gmail.com>
2023-10-08 15:07:50 +0300

    task 1 done

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,10 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_class_item():
+    res = Item("Смартфон", 10000, 20)
+    assert res.calculate_total_price() == 200000
+
+
+

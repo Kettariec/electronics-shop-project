a209b0de6166c49ec0c529fdd5f1aa17ca11c0b4
Konstantin Senkin <kettariec@gmail.com>
2023-10-10 15:28:03 +0300

    task 2 done

--- src/item.py
+++ src/item.py
@@ -1,3 +1,6 @@
+import csv
+
+
 class Item:
     """
     Класс для представления товара в магазине.
@@ -13,10 +16,10 @@ class Item:
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
         """
-        self.name = name
+        self.__name = name
         self.price = price
         self.quantity = quantity
-        self.all.append(Item)
+        self.all.append(self)
 
     def calculate_total_price(self) -> float:
         """
@@ -31,3 +34,33 @@ class Item:
         Применяет установленную скидку для конкретного товара.
         """
         self.price *= self.pay_rate
+
+    @classmethod
+    def instantiate_from_csv(cls, csv_doc):
+        """
+        Создаём экземпляры класса Item данными из файла src/items.csv
+        Добавляем экземпляры в список Item.all
+        """
+        cls.all.clear()
+        with open(csv_doc, 'r', encoding='windows-1251') as f:
+            doc = csv.DictReader(f)
+            items = []
+            for i in doc:
+                name = i['name']
+                price = float(i['price'])
+                quantity = int(i['quantity'])
+                item = cls(name, price, quantity)
+                items.append(item)
+            cls.all = items
+
+    @property
+    def name(self):
+        return self.__name
+
+    @name.setter
+    def name(self, new_name):
+        self.__name = new_name[:10]
+
+    @staticmethod
+    def string_to_number(number) -> int:
+        return int(float(number))
--- tests/test_item.py
+++ tests/test_item.py
@@ -18,6 +18,15 @@ def test_calculate_total_price(item) -> None:
 
 
 def test_apply_discount(item) -> None:
-    Item.pay_rate = 0.8
+    item.pay_rate = 0.8
     item.apply_discount()
     assert item.price == 800
+
+
+def test_name_setter():
+    item.name = 'test2'
+    assert item.name == 'test2'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5.0') == 5
